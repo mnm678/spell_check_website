8652f16f7cb529602f936dcc5ef0392b9bb4506d
marinamoore <mmoore32@calpoly.edu>
2019-11-27 12:25:05 -0800

    add database and history

--- app.py
+++ app.py
@@ -8,7 +8,11 @@ from flask_wtf.csrf import CSRFProtect
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
-from flask_login import LoginManager, UserMixin, login_required, login_user
+from flask_login import LoginManager, UserMixin, login_required, login_user, current_user
+
+from flask_sqlalchemy import SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+db = SQLAlchemy(app)
 
 app.secret_key = 'very secret'
 csrf = CSRFProtect()
@@ -18,26 +22,18 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 @login_manager.user_loader
 def load_user(user_id):
-  print("load")
-  print(user_id)
-  if (user_id in users):
-    return users[user_id]
-  return None
+  return User.query.get(user_id)
 
 #in real life, this would be random and stored in an environment variable
 SECRET_KEY = "this_is_a_secret"
 
-users = {}
+class User(db.Model):
+  __tablename__ = 'user'
 
-class User(UserMixin):
-  id = None
-  username = None
-  tfa = ""
-  password = None
-  def __init__(self, username, tfa):
-    self.username = username
-    self.tfa = tfa
-    self.id = username
+  username = db.Column(db.String, primary_key=True)
+  password = db.Column(db.String)
+  tfa = db.Column(db.String)
+  authenticated = db.Column(db.Boolean, default=False)
 
   def set_password(self, password):
     self.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
@@ -45,8 +41,37 @@ class User(UserMixin):
     return bcrypt.checkpw(password.encode('utf-8'), self.password)
   def check_tfa(self, tfa):
     return self.tfa == tfa
-  def __repr__(self):
-    return '<User {}>'.format(self.username)
+  def is_active(self):
+    return True
+  def get_id(self):
+    return self.username
+  def is_authenticated(self):
+    return authenticated
+  def is_anonymous(self):
+    return False
+
+class SpellRecord(db.Model):
+  __tablename__ = 'record'
+
+  record_id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+  user = db.Column(db.String)
+  u_query = db.Column(db.String)
+  result = db.Column(db.String)
+
+class LoginRecord(db.Model):
+  __tablename__ = 'login'
+
+  id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+  user = db.Column(db.String)
+  time = db.Column(db.Date)
+
+
+db.create_all()
+#add admin user
+admin = User(username='admin', tfa='12345678901')
+admin.set_password("Administrator@1")
+db.session.add(admin)
+db.session.commit()
 
 class RegisterForm(FlaskForm):
   uname = StringField('Username', validators=[DataRequired()])
@@ -64,11 +89,15 @@ def register():
     username = form.uname.data
     password = form.pword.data
     second_factor = form.tfa.data
-    if username in users or password == "":
+
+    user = User.query.get(username)
+
+    if user or password == "":
       return render_template("register.html", success="failure", form=form)
     user = User(username=username, tfa=second_factor)
     user.set_password(password)
-    users[username] = user
+    db.session.add(user)
+    db.session.commit()
     return render_template("register.html", success="success", form=form)
   else:
     return render_template("register.html", success="", form=form)
@@ -82,9 +111,10 @@ def login():
     password = form.pword.data
     second_factor = form.tfa.data
 
-    if username not in users:
+    user = User.query.get(username)
+
+    if not user:
       return render_template("login.html", result = "Incorrect", form=form)
-    user = users[username]
 
     if (not user.check_password(password)):
       return render_template("login.html", result = "Incorrect", form=form)
@@ -92,15 +122,27 @@ def login():
       return render_template("login.html", result = "Two-factor failure", form=form)
 
     #session management
+    user.authenticated = True
+    db.session.add(user)
+    db.session.commit()
     login_user(user)
     resp = make_response(render_template("login.html", result="success", form=form))
     return resp
   else:
     return render_template("login.html", result = "", form=form)
 
+@app.route("/logout", methods=["GET"])
+def logout():
+  user = current_user
+  user.authenticated = False
+  db.session.add(user)
+  db.session.commit()
+  logout_user(user)
+
 @app.route("/spell_check", methods=["GET", "POST"])
 @login_required
 def spell_check_page():
+  user = current_user
   form = SpellCheckForm()
 
   if form.validate_on_submit():
@@ -109,9 +151,40 @@ def spell_check_page():
     f.write(text)
     f.close()
     output = subprocess.run(["./a.out", "wordlist.txt", "file.txt"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    record = SpellRecord(user = user.username, u_query = text, result = output.stdout)
+    db.session.add(record)
+    db.session.commit()
     return render_template("spell_check.html", textout = text, misspelled = output.stdout, form=form)
   else:
     return render_template("spell_check.html", textout = "", misspelled = "", form=form)
+
+
+class HistoryForm(FlaskForm):
+  userquery = StringField('User')
+
+@app.route("/history", methods=["GET", "POST"])
+@login_required
+def get_history():
+  form = HistoryForm()
+  username = current_user.username
+  if username == "admin":
+    if form.validate_on_submit():
+      query_user = form.userquery.data
+      username = query_user
+      #fallthrough to showing the records
+    else:
+      return render_template("admin_history.html", form=form)
+
+  queries = SpellRecord.query.filter_by(user = username).all()
+  count = SpellRecord.query.filter_by(user = username).count()
+  return render_template("history.html", numqueries = count, records = queries)
+
+@app.route("/history/query<n>")
+@login_required
+def show_record(n):
+  #TODO auth
+  query = SpellRecord.query.get(n)
+  return render_template("record.html", query=query)
 
 
 if __name__ == "__main__":
